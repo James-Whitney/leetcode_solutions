4979c624d7750643cff711960b857072d62a40e6
James Whitney <jwhitn01@calpoly.edu>
2021-10-01 14:11:17 -0500

    addTwoNumbers Complete

---
+++ solutions/solution_addTwoNumbers/solution.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __eq__(self, other):
+        return self.toList() == other.toList()
+
+    def toList(self):
+        current = self
+        res = [current.val]
+        while current.next is not None:
+            current = current.next
+            res.append(current.val)
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        sum_value = 0
+        carry = 0
+        l1_next = None
+        l2_next = None
+
+        if l1:
+            l1_next = l1.next
+            sum_value += l1.val
+        if l2:
+            l2_next = l2.next
+            sum_value += l2.val
+
+        if sum_value >= 10:
+            carry = 1
+            sum_value -= 10
+
+        sum_node = ListNode(val=sum_value)
+
+        if carry:
+            if l1_next:
+                l1.next.val += carry
+            elif l2_next:
+                l2.next.val += carry
+            else:
+                sum_node.next = ListNode(val=carry)
+                return sum_node
+
+        if l1_next or l2_next:
+            sum_node.next = self.addTwoNumbers(l1_next, l2_next)
+        return sum_node
---
+++ solutions/solution_addTwoNumbers/test_solution.py
@@ -0,0 +1,45 @@
+from typing import Optional
+import unittest
+from solution import Solution
+from solution import ListNode
+
+
+def createListNode(input: list[int]) -> ListNode:
+    node_list = [ListNode(val=input[i]) for i in range(len(input))]
+    for i in range(1, len(node_list)):
+        node_list[i - 1].next = node_list[i]
+    return node_list[0]
+
+
+class TestSolution(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName=methodName)
+        self.solution = Solution()
+
+    def test_one(self):
+        input_l1 = createListNode([2, 4, 3])
+        input_l2 = createListNode([5, 6, 4])
+        output = createListNode([7, 0, 8])
+
+        actual = self.solution.addTwoNumbers(input_l1, input_l2)
+        self.assertEqual(output, actual)
+
+    def test_two(self):
+        input_l1 = createListNode([0])
+        input_l2 = createListNode([0])
+        output = createListNode([0])
+
+        actual = self.solution.addTwoNumbers(input_l1, input_l2)
+        self.assertEqual(output, actual)
+
+    def test_three(self):
+        input_l1 = createListNode([9, 9, 9, 9, 9, 9, 9])
+        input_l2 = createListNode([9, 9, 9, 9])
+        output = createListNode([8, 9, 9, 9, 0, 0, 0, 1])
+
+        actual = self.solution.addTwoNumbers(input_l1, input_l2)
+        self.assertEqual(output, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
