40c4aa25c95b20d73f045111ff65be52059454ff
James Whitney <jwhitn01@calpoly.edu>
2021-10-01 19:25:02 -0500

    Accidental copy from the the building H20 problem

--- solutions/concurrency/solution.py
+++ solutions/concurrency/solution.py
@@ -1,41 +1,13 @@
-from threading import Semaphore
-from typing import Callable
+class H2O:
+    def __init__(self):
+        pass
 
-# Solution for https://leetcode.com/problems/print-zero-even-odd/
+    def hydrogen(self, releaseHydrogen: "Callable[[], None]") -> None:
 
+        # releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen()
 
-class ZeroEvenOdd:
-    def __init__(self, n):
-        self.n = n
-        self.count = 0
-        self.zero_lock = Semaphore(1)
-        self.even_lock = Semaphore(0)
-        self.odd_lock = Semaphore(0)
+    def oxygen(self, releaseOxygen: "Callable[[], None]") -> None:
 
-    # printNumber(x) outputs "x", where x is an integer.
-    def zero(self, printNumber: "Callable[[int], None]") -> None:
-        for i in range(1, self.n + 1):
-            self.zero_lock.acquire()
-
-            printNumber(0)
-
-            if i % 2 == 1:
-                self.odd_lock.release()
-            else:
-                self.even_lock.release()
-
-    def even(self, printNumber: "Callable[[int], None]") -> None:
-        for i in range(2, self.n + 1, 2):
-            self.even_lock.acquire()
-
-            printNumber(i)
-
-            self.zero_lock.release()
-
-    def odd(self, printNumber: "Callable[[int], None]") -> None:
-        for i in range(1, self.n + 1, 2):
-            self.odd_lock.acquire()
-
-            printNumber(i)
-
-            self.zero_lock.release()
+        # releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen()
--- solutions/concurrency/solution_printZeroEvenOdd/solution.py
+++ solutions/concurrency/solution_printZeroEvenOdd/solution.py
@@ -1,19 +1,41 @@
 from threading import Semaphore
 from typing import Callable
 
-# Solution for https://leetcode.com/problems/building-h2o/
+# Solution for https://leetcode.com/problems/print-zero-even-odd/
 
 
-class H2O:
-    def __init__(self):
-        pass
+class ZeroEvenOdd:
+    def __init__(self, n):
+        self.n = n
+        self.count = 0
+        self.zero_lock = Semaphore(1)
+        self.even_lock = Semaphore(0)
+        self.odd_lock = Semaphore(0)
 
-    def hydrogen(self, releaseHydrogen: "Callable[[], None]") -> None:
+    # printNumber(x) outputs "x", where x is an integer.
+    def zero(self, printNumber: "Callable[[int], None]") -> None:
+        for i in range(1, self.n + 1):
+            self.zero_lock.acquire()
 
-        # releaseHydrogen() outputs "H". Do not change or remove this line.
-        releaseHydrogen()
+            printNumber(0)
 
-    def oxygen(self, releaseOxygen: "Callable[[], None]") -> None:
+            if i % 2 == 1:
+                self.odd_lock.release()
+            else:
+                self.even_lock.release()
 
-        # releaseOxygen() outputs "O". Do not change or remove this line.
-        releaseOxygen()
+    def even(self, printNumber: "Callable[[int], None]") -> None:
+        for i in range(2, self.n + 1, 2):
+            self.even_lock.acquire()
+
+            printNumber(i)
+
+            self.zero_lock.release()
+
+    def odd(self, printNumber: "Callable[[int], None]") -> None:
+        for i in range(1, self.n + 1, 2):
+            self.odd_lock.acquire()
+
+            printNumber(i)
+
+            self.zero_lock.release()
